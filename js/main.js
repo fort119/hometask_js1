791806d036c0234e6c36ce221d5120b056ad64e7
fort119 <i6316434@gmail.com>
2023-06-23 13:09:33 +0400

    готовая третья домашка

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Введіть перше число');
+let num2 = prompt('Введіть друге число');
+
+alert(`//Користувач ввів ${num1} і ${num2}:
+${num1}+${num2}=${+num1 + +num2}
+${num1}-${num2}=${+num1 - +num2}
+${num1}*${num2}=${+num1 * +num2}
+${num1}/${num2}=${+num1 / +num2}`);
